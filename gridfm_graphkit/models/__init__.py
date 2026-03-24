deaf640249b74e4b927cd068b7fcfcf7c81f2ce0
Thomas Tolhurst <99353435+ttolhurst@users.noreply.github.com>
2026-03-24 15:40:01 -0400

    cleanup Signed-off-by: Thomas Tolhurst <99353435+ttolhurst@users.noreply.github.com>

--- gridfm_graphkit/models/__init__.py
+++ gridfm_graphkit/models/__init__.py
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
-from gridfm_graphkit.models.gps_transformer import GPSTransformer
-from gridfm_graphkit.models.gnn_transformer import GNN_TransformerConv
-from gridfm_graphkit.models.grit_transformer import GritTransformer
 
-__all__ = ["GPSTransformer", "GNN_TransformerConv", "GRIT"]
-=======
 from gridfm_graphkit.models.gnn_heterogeneous_gns import GNS_heterogeneous
 from gridfm_graphkit.models.utils import (
     PhysicsDecoderOPF,
@@ -18,4 +12,4 @@ __all__ = [
     "PhysicsDecoderPF",
     "PhysicsDecoderSE",
 ]
->>>>>>> opensource/main
+
